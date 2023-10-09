b9d25d05dfde9d0b3c983ea52a5df1082360beb9
SOME Fiarma Landry <fiarmasome@gmail.com>
2023-10-09 11:00:40 +0000

    reconf staticfiles

--- Couture/settings.py
+++ Couture/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [  os.path.join(BASE_DIR , "Couture/static") ]
-STATICFILES_DIRS = [ BASE_DIR / "staticfiles" ]
+STATICFILES_DIRS = [ BASE_DIR / "static", "statics" ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
